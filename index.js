d04eaf283e8565ddbe0a56fc66340a34c60bac7c
Alex Davis <alex2006hw@gmail.com>
2018-10-17 01:14:02 -0700

    test-chain

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 'use strict';
+var Gun = require('gun');
+require('gun/lib/not.js');
+require('gun/lib/path.js');
+require('gun-unset');
+
+var peers = ['troposphere.usertoken.com']
 
 /**
- * Adds commas to a number
- * @param {number} number
+ * Blockchain Contract for CRUD on memories
+ * @param {number} contractID
  * @param {string} locale
  * @return {string}
  */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
+
+module.exports = function(contractID, locale) {
+    var globalChain = Gun({
+        peers: peers,
+        radisk: false,
+        file: false
+    })
+    var contractChain = globalChain.get(contractID)
+    var answerChain = globalChain.get(contractID+'/answer')
+    var answer = contractID.toLocaleString(locale)
+    //
+    contractChain.put({contractID: contractID})
+    answerChain.put({answer: answer})
+    // contractChain.path('answer').set(answerChain)
+    return {contractChain: contractChain, answerChain: answerChain};
 };
--- test/test.js
+++ test/test.js
@@ -1,46 +1,42 @@
 'use strict';
+var Gun = require('gun');
+require('gun/lib/not.js');
+require('gun/lib/path.js');
+require('gun-unset');
 
 var expect = require('chai').expect;
-var numFormatter = require('../index');
+var tokenMemory = require('../index');
 
-describe('#numFormatter', function() {
-    it('should convert single digits', function() {
-        var result = numFormatter(1);
-        expect(result).to.equal('1');
+var peers = ['troposphere.usertoken.com']
+var globalChain = Gun({
+    peers: peers,
+    radisk: false,
+    file: false
+})
+
+
+
+tokenMemory(1);
+////////
+describe('#tokenMemory', function() {
+    it('should convert single digits and have answer in answerChain', function() {
+        // var contractChain = globalChain.get(contractID)
+        // var answerChain = result.answerChain
+        // var contractChain = result.contractChain
+
+        var answerChain = globalChain.get('1/answer')
+        answerChain.get('answer').on(function(data, key){
+            expect(data).to.equal('1');
+        });
     });
 
-    it('should convert double digits', function() {
-        var result = numFormatter(12);
-        expect(result).to.equal('12');
-    });
+    it('should convert single digits and have contractID in contractChain', function() {
+        // var answerChain = result.answerChain
 
-    it('should convert triple digits', function() {
-        var result = numFormatter(123);
-        expect(result).to.equal('123');
-    });
-
-    it('should convert 4 digits', function() {
-        var result = numFormatter(1234);
-        expect(result).to.equal('1,234');
-    });
-
-    it('should convert 5 digits', function() {
-        var result = numFormatter(12345);
-        expect(result).to.equal('12,345');
-    });
-
-    it('should convert 6 digits', function() {
-        var result = numFormatter(123456);
-        expect(result).to.equal('123,456');
-    });
-
-    it('should convert 7 digits', function() {
-        var result = numFormatter(1234567);
-        expect(result).to.equal('1,234,567');
-    });
-
-    it('should convert 8 digits', function() {
-        var result = numFormatter(12345678);
-        expect(result).to.equal('12,345,678');
+        var contractChain = globalChain.get('1')
+        contractChain.get('contractID').on(function(data, key){
+            expect(data).to.equal('1');
+        });
     });
 });
+////////
