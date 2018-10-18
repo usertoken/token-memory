a67c7021ec096980f28740c8b79bcd0bb3b13ca3
alex Davis <alex@alex2006hw.com>
2018-10-18 00:22:22 -0700

    token

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-'use strict';
-var Gun = require('gun');
-require('gun/lib/not.js');
-require('gun/lib/path.js');
-require('gun-unset');
+"use strict";
 
-var peers = ['troposphere.usertoken.com']
-var tokenMemory = Gun({
-    peers: peers,
-    radisk: false,
-    // file: 'radata'
-})
-
+var Token = require("./lib/token");
 /**
  * Blockchain Contract for CRUD on memories
  * @param {string} chain
- * @param {string} locale
- * @return {string}
+ * @return {json}
  */
 
-module.exports = function(chain, locale) {
-    var contractChain = tokenMemory.get(chain)
-    var answerChain = tokenMemory.get(chain+'/answer')
-    // var answer = chain.toLocaleString(locale)
-    //
-    contractChain.get('chain').put(chain)
-    answerChain.get('chain').put(chain)
-    // contractChain.path('answer').set(answerChain)
-    var answer = {contractChain: contractChain, answerChain: answerChain};
-    return answer
+module.exports = function(chain) {
+  var chains = Token.create(chain);
+  //   var { chain } = chains;
+  //   chain.get("tests").put(chain);
+  return chains;
 };
--- test/test.js
+++ test/test.js
@@ -1,42 +1,19 @@
-'use strict';
-var Gun = require('gun');
-require('gun/lib/not.js');
-require('gun/lib/path.js');
-require('gun-unset');
+"use strict";
 
-var expect = require('chai').expect;
-var Chain = require('../index');
-
-var peers = ['troposphere.usertoken.com']
-var tokenMemory = Gun({
-    peers: peers,
-    radisk: false,
-    // file: 'radata'
-})
+var UUIDv1 = require("uuid/v1")();
+var expect = require("chai").expect;
+var Token = require("../index");
 
 ////////
-describe('#tokenMemory', function() {
-    it('should have answer in answerChain', function() {
-        // var contractChain = tokenMemory.get(contractID)
-        // var answerChain = result.answerChain
-        // var contractChain = result.contractChain
-        var chainID = 1234567
-        var result = Chain(chainID);
-        var answerChain = tokenMemory.get(chainID+'/answer')
-        answerChain.get('chain').once(function(data, key){
-            return expect(data).to.equal(chainID);
-        });
+describe("#chainFunctions", () => {
+  it(`should have create new chain : ${UUIDv1}`, () => {
+    var chains = Token(UUIDv1);
+    var { chain } = chains;
+    var testChain = chain.get("tests");
+    testChain.put(chain);
+    testChain.once((data, key) => {
+      return expect(data).to.equal(UUIDv1);
     });
-
-    it('should have contractID in contractChain', function() {
-        // var answerChain = result.answerChain
-        var chainID = 12345678
-        var result = Chain(chainID);
-        var contractChain = tokenMemory.get(chainID)
-        contractChain.get('chain').once(function(data, key){
-            return expect(data).to.equal(chainID);
-        });
-    });
-    return
+  });
 });
 ////////
